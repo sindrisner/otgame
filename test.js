2937980e466189c01b490f86bbc59fda5930d598
Sindri Snær Björnsson <Bjornsson.sindri@gmail.com>
2022-10-19 12:56:25 +0200

    add testing gyro

---
+++ test.js
@@ -0,0 +1,15 @@
+window.addEventListener("deviceorientation", function (e) {
+  const x = Math.round(e.beta);
+  const y = Math.round(e.gamma);
+  const z = Math.round(e.alpha);
+
+  document.getElementsByClassName("xtxt")[0].innerText = "X-Axis = " + x;
+  document.getElementsByClassName("ytxt")[0].innerText = "Y-Axis = " + y;
+  document.getElementsByClassName("ztxt")[0].innerText = "Z-Axis = " + z;
+
+  //Parallax Effect
+
+  document.getElementsByClassName(
+    "rocket"
+  )[0].style.transform = `translateY(-${x}%)`;
+});
