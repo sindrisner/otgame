b3c918f77915e72445a3d12f4a16946739270b74
Sindri Snær Björnsson <Bjornsson.sindri@gmail.com>
2022-10-20 12:53:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,37 +28,17 @@ document.addEventListener("keydown", (event) => {
   }
 });
 
-const moveRightGeo = (number) => {
-  let left = parseInt(
-    window.getComputedStyle(character).getPropertyValue("left")
-  );
-  left += 100 * 2.4;
-  if (left < 600 * 2.4) {
-    character.style.left = left + "px";
-  }
-};
-
-const moveLeftGeo = (number) => {
-  let left = parseInt(
-    window.getComputedStyle(character).getPropertyValue("left")
-  );
-  left -= 100 * 2.4;
-  if (left >= 0) {
-    character.style.left = left + "px";
-  }
-};
-
 window.addEventListener("deviceorientation", function (e) {
   const x = Math.round(e.beta);
   const y = Math.round(e.gamma);
   const z = Math.round(e.alpha);
 
   if (y < -17) {
-    moveLeftGeo(y);
+    moveLeft();
   }
 
   if (y > 17) {
-    moveRightGeo(y);
+    moveRight();
   }
 
   //Parallax Effect
@@ -145,7 +125,6 @@ setInterval(() => {
   }
 }, 100);
 // };
-
 window.addEventListener("deviceorientation", function (e) {
   const x = Math.round(e.beta);
   const y = Math.round(e.gamma);
