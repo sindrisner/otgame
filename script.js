0bb5571f85c48d425a6181aa384b939f51b0da34
Sindri Snær Björnsson <Bjornsson.sindri@gmail.com>
2022-10-25 09:26:02 +0200

    test

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ const moveRightGeo = (number) => {
     window.getComputedStyle(character).getPropertyValue("left")
   );
   left += number;
-  if (left < 500 * 2.4) {
+  if (left >= 0) {
     character.style.left = left + "px";
   }
 };
